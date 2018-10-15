9fb7252ad4972b12d920e3696ac330584f4723fb
JesusLaiseca <jesuslaiseca@gmail.com>
2018-10-15 17:04:18 +0200

    inverse

--- js/JavaSript.js
+++ js/JavaSript.js
@@ -1,3 +1,8 @@
+function inverse(){
+var num = document.getElementById("n1");
+num.value = 1/num.value;
+}
+
 function square(){
  var num = document.getElementById("n1");
  num.value = num.value * num.value;
